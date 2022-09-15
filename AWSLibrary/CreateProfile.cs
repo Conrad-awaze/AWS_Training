caa8e308261adb6a1970c8e8353c38968d8aff18
Conrad <Conrad@HV-WIN10>
2022-09-15 14:24:26 +0100

    Add project files.

---
+++ AWSLibrary/CreateProfile.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Runtime.CredentialManagement;
+
+namespace AWSLibrary;
+
+public class CreateProfile
+{
+    public static void WriteProfile(string profileName, string keyId, string secret)
+    {
+        Console.WriteLine($"Creating the [{profileName}] profile...!!");
+        var options = new CredentialProfileOptions
+        {
+            AccessKey = keyId,
+            SecretKey = secret
+        };
+        var profile = new CredentialProfile(profileName, options);
+        var sharedFile = new SharedCredentialsFile();
+        sharedFile.RegisterProfile(profile);
+        Console.WriteLine($"Profile [{profileName}] has now been created");
+        Console.ReadLine();
+    }
+}
---
+++ SetupAWSCredentials/Program.cs
@@ -0,0 +1,13 @@
+﻿using AWSLibrary;
+
+Console.Write("Enter the Profile Name: ");
+string? profileName = Console.ReadLine();
+
+Console.Write("Enter the AccesKeyID: ");
+string? keyId = Console.ReadLine();
+
+Console.Write("Enter the Secret AccesKey: ");
+string? secret = Console.ReadLine();
+
+CreateProfile.WriteProfile(profileName, keyId, secret);
+Console.ReadLine();
